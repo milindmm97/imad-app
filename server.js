d01f02264b741f672b9d20c10f3ef686215dd18b
milindmm97 <milindmmanoj@outlook.com>
2017-08-17 20:59:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function (req, res) {
     
-    res.sendFile(createTemple(articleOne));
+    res.send(createTemple(articleOne));
 });
 
 app.get('/article-two',function(req, res){
