a3ff2aa0f1e3bdec12b628b2dc8c52bca234b199
milindmm97 <milindmmanoj@outlook.com>
2017-08-17 21:01:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var articleOne= {
     
     
 };
-function createTemple (data) {
+function createTemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function (req, res) {
     
-    res.send(createTemple(articleOne));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req, res){
