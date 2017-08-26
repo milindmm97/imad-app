0c829618a25f3d5dc6e706dc45f50ddcbca16c8f
Milind M <milindmmanoj@outlook.com>
2017-08-26 21:25:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var couner=0;
+button.onClick = function(){
+    
+    
+    //render variable
+    counter = couner +1;
+    var span = document.getElementById('counter');
+    span.innerHTML = counter.toString();
+    
+}
