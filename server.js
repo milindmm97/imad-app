4289b46007d8e205774ab890cb71e93c3ad61385
Milind M <milindmmanoj@outlook.com>
2017-08-26 21:13:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/', function (req, res) {
 
 var counter=0;
 app.get('/counter',function(res,req){
-    counter = counter+1;
+    counter = counter + 1;
     res.send(counter.toString());
     
 });
